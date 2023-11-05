18b444513c1ecc3d1364acc517e7d54dc57186aa
Ygor Torres <ygorftf@gmail.com>
2023-11-05 08:08:11 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/ygorftf/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ygorftf/workshopmongo/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.ygorftf.workshopmongo.resources;
 
 import com.ygorftf.workshopmongo.domain.User;
+import com.ygorftf.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +17,12 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-
-        User maria = new User("1","Maria Silva", "maria@gmail.com");
-        User alex = new User("2","Alex Green", "alex@gmail.com");
-        List<User>  list = new ArrayList<>();
-
-        list.addAll(Arrays.asList(maria, alex));
+        List<User> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
