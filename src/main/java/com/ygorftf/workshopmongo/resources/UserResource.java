e97da6d45b13237f2cc0549e66636c0d6cfe1951
Ygor Torres <ygorftf@gmail.com>
2023-11-05 10:31:49 -0300

    Padrão DTO

--- src/main/java/com/ygorftf/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ygorftf/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.ygorftf.workshopmongo.resources;
 
 import com.ygorftf.workshopmongo.domain.User;
+import com.ygorftf.workshopmongo.dto.UserDTO;
 import com.ygorftf.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -21,8 +23,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
