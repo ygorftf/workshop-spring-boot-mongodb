1fe64b719f400d9333ada32a105b5d907221d955
Ygor Torres <ygorftf@gmail.com>
2023-11-06 06:28:59 -0300

    Obtendo um usuario por id

--- src/main/java/com/ygorftf/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ygorftf/workshopmongo/resources/UserResource.java
@@ -5,10 +5,7 @@ import com.ygorftf.workshopmongo.dto.UserDTO;
 import com.ygorftf.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,5 +24,10 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
--- src/main/java/com/ygorftf/workshopmongo/services/UserService.java
+++ src/main/java/com/ygorftf/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.ygorftf.workshopmongo.services;
 
 import com.ygorftf.workshopmongo.domain.User;
 import com.ygorftf.workshopmongo.repository.UserRepository;
+import com.ygorftf.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +16,10 @@ public class UserService {
     private UserRepository repo;
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto Não Encontrado"));
     }
 }
