1a7fe7b9c5290e05ff5c1da617496482afcc51b4
Ygor Torres <ygorftf@gmail.com>
2023-11-09 06:15:26 -0300

    Referenciando os posts do usuário

--- src/main/java/com/ygorftf/workshopmongo/config/Instantiation.java
+++ src/main/java/com/ygorftf/workshopmongo/config/Instantiation.java
@@ -42,5 +42,8 @@ public class Instantiation implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
