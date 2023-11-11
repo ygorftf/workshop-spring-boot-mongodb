8cbf595abea3e74a92f846746f8ec945b7970ada
Ygor Torres <ygorftf@gmail.com>
2023-11-11 07:10:47 -0300

    Consulta com @Query

--- src/main/java/com/ygorftf/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/ygorftf/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.ygorftf.workshopmongo.repository;
 import com.ygorftf.workshopmongo.domain.Post;
 import com.ygorftf.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ title: { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
